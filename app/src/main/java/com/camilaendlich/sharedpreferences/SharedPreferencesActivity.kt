1734d3bc69ed52b33df649bbf13cf45904933dc2
Caca Endlich <camilaendlichpimenta.trab@gmail.com>
2024-02-23 16:15:28 -0300

    up

--- app/src/main/java/com/camilaendlich/sharedpreferences/SharedPreferencesActivity.kt
+++ app/src/main/java/com/camilaendlich/sharedpreferences/SharedPreferencesActivity.kt
@@ -6,10 +6,8 @@ package com.camilaendlich.sharedpreferences
  *classes relacionadas ao sistema Android e a classe de ligação (binding) para a atividade.*/
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.camilaendlich.sharedpreferences.databinding.ActivitySharedPreferencesBinding
 
@@ -31,53 +29,53 @@ class SharedPreferencesActivity : AppCompatActivity() {
         binding = ActivitySharedPreferencesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        gravarButton = binding.btnGravar
-        nomeEditText = binding.nome
-        telefoneEditText = binding.telefone
-        idadeEditText = binding.idade
+        gravarButton = binding.btnSave
+        nomeEditText = binding.name
+        telefoneEditText = binding.tel
+        idadeEditText = binding.age
 
         // Define o foco para o EditText do nome quando a atividade é criada.
         nomeEditText.requestFocus()
 
         // Lógica para preencher os EditText com os valores dos SharedPreferences se existirem
         if (checarPrefsIsNotEmpty()){
-            val (name, telefone, idade) = consultarSharedPrefs()
-            preencherInputs(name, telefone, idade)
-            Toast.makeText(this, "Bem vindo de volta!", Toast.LENGTH_LONG).show()
+            val (name, tel, age) = consultarSharedPrefs()
+            preencherInputs(name, tel, age)
+            Toast.makeText(this, "Welcome back!", Toast.LENGTH_LONG).show()
         }
 
         gravarButton.setOnClickListener {
             // Lógica para gravar os dados nos SharedPreferences quando o botão é clicado
-                val editor = getSharedPreferences("minhas_preferences", MODE_PRIVATE).edit()
+                val editor = getSharedPreferences("my_prefs", MODE_PRIVATE).edit()
 
-                editor.putString("nome", nomeEditText.text.toString())
-                editor.putString("telefone", telefoneEditText.text.toString())
-                editor.putString("idade", idadeEditText.text.toString())
+                editor.putString("name", nomeEditText.text.toString())
+                editor.putString("tel", telefoneEditText.text.toString())
+                editor.putString("age", idadeEditText.text.toString())
 
                 editor.apply()
 
-                Toast.makeText(this, "Dados gravados com sucesso!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Data recorded successfully!", Toast.LENGTH_LONG).show()
         }
 
     }
 
     private fun checarPrefsIsNotEmpty(): Boolean {
-        val sharedPreferences = getSharedPreferences("minhas_preferences", MODE_PRIVATE)
-        return sharedPreferences.contains("nome")
+        val sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE)
+        return sharedPreferences.contains("name")
     }
 
     private fun consultarSharedPrefs(): Triple<String, String, String> {
         //O Triple em Kotlin é uma classe que permite armazenar três valores em uma única estrutura de dados
-        val sharedPreferences = getSharedPreferences("minhas_preferences", MODE_PRIVATE)
-        val name = sharedPreferences.getString("nome", "") ?: ""
-        val telefone = sharedPreferences.getString("telefone","") ?: ""
-        val idade = sharedPreferences.getString("idade", "") ?: ""
+        val sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE)
+        val name = sharedPreferences.getString("name", "") ?: ""
+        val telefone = sharedPreferences.getString("tel","") ?: ""
+        val idade = sharedPreferences.getString("age", "") ?: ""
         return Triple(name, telefone, idade)
     }
 
-    private fun preencherInputs(nome: String, telefone: String, idade: String) {
-        nomeEditText.setText(nome)
-        telefoneEditText.setText(telefone)
-        idadeEditText.setText(idade)
+    private fun preencherInputs(name: String, tel: String, age: String) {
+        nomeEditText.setText(name)
+        telefoneEditText.setText(tel)
+        idadeEditText.setText(age)
     }
 }
